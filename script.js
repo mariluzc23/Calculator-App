08dc0523d991a1f49597cf5d0c2ecc0206f4eda2
Mariluz <mariluz.colon23@gmail.com>
2023-02-06 22:23:29 -0500

    updated calculator

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ class Calculator {
             case '-':
                 computation = prev - current
                     break
-            case '*':
+            case 'x':
                 computation = prev * current
                 break
             case '÷':
